e6aeb2ef852f323862e8343f68e6e8f88e49f12f
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-01 16:51:10 +0000

    Adding support for shortlink expansion. Depends on patch idno/idno#547

--- Gist/templates/default/gist/embed.tpl.php
+++ Gist/templates/default/gist/embed.tpl.php
@@ -1,7 +1,9 @@
 <?php
 
 $embedded = '';
-if (preg_match_all('/gist\.github\.com\/([^\/]+\/[0-9a-f]+)/i', $vars['object']->body, $matches)) {
+$body = Idno\Core\site()->triggerEvent('url/expandintext', ['object' => $vars['object']], $vars['object']->body); 
+
+if (preg_match_all('/gist\.github\.com\/([^\/]+\/[0-9a-f]+)/i', $body, $matches)) {
     foreach ($matches[1] as $m)
         $embedded .= '<script src="https://gist.github.com/' . $m . '.js"></script>';
 }
